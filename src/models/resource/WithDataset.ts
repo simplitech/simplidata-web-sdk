4c00e17e40133900171d6cd506b8a0b7402f561f
Gil Lopes Bueno <gil.bueno@martinlabs.com.br>
2018-11-30 14:08:49 -0200

    feat(Chart): legend, more options for hiding parts, event on click to visit; WithDataset removed; bu
    fix MCR-156

--- src/models/resource/WithDataset.ts
+++
@@ -1,6 +0,0 @@
-import { Resource } from 'simpli-web-sdk'
-import { OaDataset } from './OaDataset'
-
-export abstract class WithDataset extends Resource {
-  abstract $dataset(idVersion: number): OaDataset
-}
